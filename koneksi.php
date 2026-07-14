315942708d614a1fef3aa3937354e914f90362e5
JALL1810 <lija18101@gmail.com>
2026-07-15 00:17:40 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,12 +1,12 @@
 <?php
-$host = getenv('mysql');
-$user = getenv('root');
-$pass = getenv('LWfTBEhhjuTvIPJHrtJaWaXdkLRNvWfw');
-$db   = getenv('railway');
-$port = getenv('3306');
+$host = "mysql";
+$user = "root";
+$pass = "LWfTBEhhjuTvIPJHrtJaWaXdkLRNvWfw";
+$db   = "railway";
+$port = "3306";
 
 try {
-    // Kita pakai pa9rameter port karena Railway tidak pakai port default standar
+    // Kita pakai parameter port karena Railway tidak pakai port default standar
     $koneksi = new PDO("mysql:host=$host;port=$port;dbname=$db", $user, $pass);
     $koneksi->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
